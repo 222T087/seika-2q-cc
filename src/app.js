ef1dcdd5cd5a569522408beba0161a36375dd97f
shiraishi.Tatsuya <s222t087@st.kyoto-seika.ac.jp>
2024-07-18 15:06:30 +0900

    ファイルを追加した

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//import anime from 'animejs'
+import Aos from 'aos';
+
+Aos.init({
+  offset: 100,
+  delay: 200,
+  duration: 500,
+  easing: 'ease-out',
+  anchorPlacement: 'bottom-top',
+  once: false
+});
+
+/*let elem = document.getElementById('elem');
+elem.addEventListener('click',function(){
+  anime({
+    targets: elem,
+    translateX: 250                
+  })              
+})*/
+
+console.log("これはappのコードです。");
