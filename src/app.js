d55b0b59cc9fb90b26eaf94fa2c40469baa285e4
Tatsuya.Shiraishi <s222t087@st.kyoto-seika.ac.jp>
2025-07-18 14:17:12 +0900

    二つ目の動画を追加

--- src/app.js
+++ src/app.js
@@ -5,12 +5,28 @@ AOS.init({
   delay: 500,
 });
 var player1;
+var player2;
 
 window.onYouTubeIframeAPIReady = function () {
   player1 = new YT.Player('movie1', {
     videoId: 'S_msdXWYQ38',
     playerVars: {
+      autoplay: 0,
       mute: 1,
+      controls: 1,
+      rel: 0,
+      modestbranding: 1,
+    },
+  });
+
+  player2 = new YT.Player('movie2', {
+    videoId: 'GyMj5K4g0H4',
+    playerVars: {
+      autoplay: 0,
+      mute: 1,
+      controls: 1,
+      rel: 0,
+      modestbranding: 1,
     },
   });
 };
